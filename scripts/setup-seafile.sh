1fd5d3a66c3450ef05ed59d88b896ce51aa68c22
kasperekd <kasperekd@mail.ru>
2026-03-03 18:31:59 +0700

    seafile update

--- scripts/setup-seafile.sh
+++ scripts/setup-seafile.sh
@@ -8,8 +8,14 @@ set_hostname_and_hosts "seafile" "$SEAFILE_IP"
 apply_netplan_static "$SEAFILE_IP"
 
 log "Установка зависимостей Seafile и MariaDB..."
-apt-get install -y -qq python3 python3-setuptools python3-pip libmysqlclient-dev mariadb-server nginx
-pip3 install --timeout=3600 django==3.2.* Pillow pylibmc captcha jinja2 sqlalchemy==1.4.3 django-pylibmc django-simple-captcha python3-ldap mysqlclient pycryptodome==3.12.0 cffi==1.14.0 --break-system-packages
+apt-get install -y -qq python3 python3-dev python3-setuptools python3-pip libmysqlclient-dev ldap-utils libldap2-dev python3.12-venv default-libmysqlclient-dev build-essential pkg-config libmemcached-dev libmysqlclient-dev mariadb-server nginx
+
+python3 -m venv python-venv
+source python-venv/bin/activate
+
+pip3 install --timeout=3600 django==4.2.* future==1.0.* mysqlclient==2.2.* \
+    pymysql pillow==10.4.* pylibmc captcha==0.6.* markupsafe==2.0.1 jinja2 sqlalchemy==2.0.* \
+    psd-tools django-pylibmc django_simple_captcha==0.6.* djangosaml2==1.9.* pysaml2==7.3.* pycryptodome==3.20.* cffi==1.17.0 lxml python-ldap==3.4.* gevent==24.2.*
 
 systemctl enable --now mariadb
 mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root'; FLUSH PRIVILEGES;" || true
@@ -17,14 +23,14 @@ mysql -u root -e "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='ro
 log "Установка Seafile Server..."
 mkdir -p /opt/seafile
 cd /opt/seafile
-if [ ! -f seafile-server_9.0.9_x86-64.tar.gz ]; then
-    wget -q https://s3.eu-central-1.amazonaws.com/download.seadrive.org/seafile-server_9.0.9_x86-64.tar.gz
-    tar -xzf seafile-server_9.0.9_x86-64.tar.gz
+if [ ! -f seafile-server_12.0.14_x86-64.tar.gz ]; then
+    wget -q https://s3.eu-central-1.amazonaws.com/download.seadrive.org/seafile-server_12.0.14_x86-64.tar.gz
+    tar -xzf seafile-server_12.0.14_x86-64.tar.gz
 fi
 
 # Если ранее не настраивали
 if [ ! -d /opt/seafile/seafile-data ]; then
-    cd seafile-server-9.0.9
+    cd seafile-server_12.0.14
     # Автоматическая настройка (имитация действий скрипта setup-seafile-mysql.sh)
     ./setup-seafile-mysql.sh auto -n "seafile" -i "$SEAFILE_IP" -p 8082 -d "/opt/seafile/seafile-data" -e 0 -u "root" -w "" -c "ccnet" -s "seafile" -b "seahub" || log "Предупреждение при автоустановке БД"
 fi
@@ -57,8 +63,8 @@ After=mariadb.service network.target
 
 [Service]
 Type=forking
-ExecStart=/opt/seafile/seafile-server-9.0.9/seafile.sh start
-ExecStop=/opt/seafile/seafile-server-9.0.9/seafile.sh stop[Install]
+ExecStart=/opt/seafile/seafile-server-12.0.14/seafile.sh start
+ExecStop=/opt/seafile/seafile-server-12.0.14/seafile.sh stop[Install]
 WantedBy=multi-user.target
 EOF
 
@@ -69,8 +75,8 @@ After=seafile.service
 
 [Service]
 Type=forking
-ExecStart=/opt/seafile/seafile-server-9.0.9/seahub.sh start
-ExecStop=/opt/seafile/seafile-server-9.0.9/seahub.sh stop
+ExecStart=/opt/seafile/seafile-server-12.0.14/seahub.sh start
+ExecStop=/opt/seafile/seafile-server-12.0.14/seahub.sh stop
 
 [Install]
 WantedBy=multi-user.target
